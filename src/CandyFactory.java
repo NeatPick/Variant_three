1cf0edda793fde08f4a461f5fdce14410075b645
NeatPick <yacovlew2002@yandex.ru>
2025-02-10 22:03:54 +0800

    Task 6

---
+++ src/CandyFactory.java
@@ -0,0 +1,82 @@
+
+interface Candies {
+    String Manufacturer(); // Производитель
+    String Info();// Информация о продукте
+    void produce(int count);
+    void editingInfo(String newInfo); //Редактирование информации о продукте
+}
+
+class Candy implements Candies{
+    String name;
+    String manufacturer;
+    String info;
+
+    Candy(String name, String manufacturer) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+    }
+
+    public void produce(int count) {
+        System.out.println("Произведено " + count + " шт. " + name);
+    }
+
+    public String Manufacturer(){
+        return  "Производитель: " + manufacturer;
+    }
+    public void editingInfo(String newInfo){
+        this.info = newInfo;
+        System.out.println("Информация изменена для " + name + ": " + newInfo);
+    }
+
+    public String Info(){
+        return  "Информация о " + name + ": " + info;
+    }
+}
+
+class Chocolate extends Candy {
+    Chocolate(String manufacturer) {
+       super("Шоколад", manufacturer);
+
+    }
+}
+
+class Lollipop extends Candy {
+    Lollipop(String manufacturer) {
+        super("Леденец", manufacturer);
+    }
+}
+
+class Caramel extends Candy {
+    Caramel(String manufacturer) {
+        super("Карамель", manufacturer);
+    }
+
+
+}
+
+public class CandyFactory{
+    public static void CandyInfo(){
+        Chocolate chocolate = new Chocolate("Победа");
+        chocolate.produce(100);
+        chocolate.editingInfo("Горький шоколад 72% какао");
+        System.out.println(chocolate.Manufacturer());
+        System.out.println(chocolate.Info());
+
+        Lollipop lollipop = new Lollipop("Кармолис");
+        lollipop.produce(50);
+        lollipop.editingInfo("Леденец со имбирь-лимон");
+        System.out.println(lollipop.Manufacturer());
+        System.out.println(lollipop.Info());
+
+        Caramel caramel = new Caramel("Яшкино");
+        caramel.produce(300);
+        caramel.editingInfo("Карамель со вкусом яблоко с соком");
+        System.out.println(caramel.Manufacturer());
+        System.out.println(caramel.Info());
+    }
+}
+
+
+
+
+
--- src/VariantThree.java
+++ src/VariantThree.java
@@ -2,16 +2,39 @@
                        ВАРИАНТ 3
 */
 
+import java.util.Scanner;
+
 public class VariantThree {
 
     public static void main(String[] args) {
-        // TaskOne.RandomOfNumbers();
-        // TaskTwo.LengthOfNumbers();
-        // PatientManager.workingWithPatients();
-        //AirplaneManager.airplaneControl();
-        MyAccount.myAccount();
+        itemMenu();
+    }
+
+    private static void itemMenu(){
+        System.out.println("Лабораторные работы по Java.");
+        System.out.println("Выполнил Яковлев Александр студент группы ЗБ 762. ВАРИАНТ 3");
+        int item;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Выберите задание, для тестирования (1-10): ");
+        item = scan.nextInt();
+        switch (item) {
+            case 1: TaskOne.randomOfNumbers();
+            break;
+            case 2: TaskTwo.lengthOfNumbers();
+            break;
+            case 3: PatientManager.workingWithPatients();
+            break;
+            case 4: AirplaneManager.airplaneControl();
+            break;
+            case 5: MyAccount.myAccount();
+            break;
+            case 6: CandyFactory.CandyInfo();
+            break;
+            default: System.out.println("Неверно выбран номер задания!");
+        }
     }
 }
+
 
 
 
